1c695b227b0749cc3b682f0254959a45f8676560
Yusuf Mert <ercetinyusufmert6@gmail.com>
2026-03-29 20:01:14 +0300

    devam

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,28 +3,19 @@ import "../scss/header.scss";
 import Link from 'next/link';
 import Image from 'next/image';
 import LOGO from '../../public/logo_beyaz.png'
-import { useState } from "react";
-
-interface control{
-  basti: boolean;
-  setBasti: (val:boolean) => boolean;
-}
 
 export default function Header() {
-
-  const [basti, setBasti] = useState<boolean>(false);
 
 return(
   <header>
 
-    <Image src={LOGO} alt="Logo" width={150} height={150} className="ml-10" />
-
     <nav>
-
     <Link className="pagelinks" href="/">Ana Sayfa</Link>
-    <button className="pagelinks">Giriş Yap</button>
+    <Link className="pagelinks" href="#">Geliştiriciler</Link>
+    <Link className="pagelinks" href="../Loginpage">Giriş Yap</Link>
     </nav>
   </header>
 );
+
 }
 
--- app/components/user-log-reg.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Userlog() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
